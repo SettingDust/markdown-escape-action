7ebf16ce2e69f19e20dc24fad9cb62654d90df53
SettingDust <settingdust@gmail.com>
2022-06-24 19:06:28 +0800

    fix:  output can't be number

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function run(): Promise<void> {
       (core.getInput('type') as 'html' | 'markdown' | 'markdownv2') ??
       'markdownv2'
     for (let i = 0; i < inputs.length; i++)
-      core.setOutput(i.toString(), formats[type].escape(inputs[i]))
+      core.setOutput(`group${i.toString()}`, formats[type].escape(inputs[i]))
   } catch (error) {
     core.setFailed(error.message)
   }
