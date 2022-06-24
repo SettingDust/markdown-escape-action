c633d1c40411a15e88d00393c74cf6c95d58f87e
SettingDust <settingdust@gmail.com>
2022-06-24 17:27:34 +0800

    feat: add default to type

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import * as formats from 'telegram-format'
 async function run(): Promise<void> {
   try {
     const inputs = core.getInput('text').split('\n')
-    const type = core.getInput('type') as 'html' | 'markdown' | 'markdownv2'
+    const type =
+      (core.getInput('type') as 'html' | 'markdown' | 'markdownv2') ??
+      'markdownv2'
     for (let i = 0; i < inputs.length; i++)
       core.setOutput(i.toString(), formats[type].escape(inputs[i]))
   } catch (error) {
